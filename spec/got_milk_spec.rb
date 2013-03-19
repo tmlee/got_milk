c00dff90f24de80c22b1f2ced44791d3ec36f390
TM Lee <tm89lee@gmail.com>
2013-03-19 22:01:35 +0800

    rspec expect with milk text

--- spec/got_milk_spec.rb
+++ spec/got_milk_spec.rb
@@ -1,7 +1,23 @@
 require 'spec_helper'
 
 describe GotMilk do
-  it 'should return correct version string' do
-    true.should == true
-  end
+  
+	context "testing for existence of Milk" do
+
+		it "is true for text with milk" do
+			GotMilk.got_milk?("Quick brown fox jumps over the milk").should be_true
+		end
+
+		it "is true for text with MiLk" do
+			GotMilk.got_milk?("Quick brown fox jumps over the MiLk!").should be_true
+		end
+
+		it "is false for text with no milk" do
+			GotMilk.got_milk?("Quick brown fox jumps over the monk").should be_false
+		end
+
+	end
+
+
+
 end
